661d4f5d5cca387a5a8de851d72d59520acf613a
Danillo Liz Mendes Mailan Silva <danillo.silva@al.infnet.edu.br>
2024-03-21 21:19:23 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoDeLaboratorios/DAL/InfnetDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GestaoDeLaboratorios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoDeLaboratorios.DAL
+{
+    public class InfnetDbContext : DbContext
+    {
+        public InfnetDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Computador> Computadores { get; set; }
+
+    }
+}
---
+++ GestaoDeLaboratorios/Models/Computadores.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computador
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Processador { get; set; }
+        public string PlacaMae { get; set; }
+        public string Memoria { get; set; }
+        public string HD { get; set; }
+        public string Numero { get; set; }
+        public string DataDeCompra {  get; set; }
+    }
+}
